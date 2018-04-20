cb5aa0821569676416074a3caf9e0d6c72ae52a1
maxchang39 <maxchang39@yahoo.com>
2018-04-20 16:40:20 -0700

    rename project and clean up

---
+++ src/main/java/edu/neu/maxchang39/teamin/config/AppConfig.java
@@ -0,0 +1,12 @@
+package edu.neu.maxchang39.teamin.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "net.viralpatel.spring")
+public class AppConfig {
+
+}
